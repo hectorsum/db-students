98c85b24b994c952b45cf75aa25df1197b03599c
Hector Herrera <hectorsum81@hotmail.com>
2020-05-16 03:49:38 -0500

    Update-function was updated...

--- SQLite.py
+++ SQLite.py
@@ -30,28 +30,30 @@ class student:
         tk.Label(win,text="Ciclo: ").grid(row=3,column=2,sticky='W',ipadx=5)
 
         ################# TextBoxes ####################
-        self.state = "readonly"
-        #self.normal = "normal"
+        self.read = "readonly"
+        self.normal = "normal"
         #self.codigo = tk.Entry(win)
         # self.codigo.grid(row=1,column=1,sticky="W")
-        self.nombre = tk.Entry(win,state=self.state) #Nombres
+        self.nombre = tk.Entry(win,state=self.read) #Nombres
         self.nombre.grid(row=1,column=1,sticky="W")
-        self.apell = tk.Entry(win) #Apellidos
+        self.apell = tk.Entry(win,state=self.read) #Apellidos
         self.apell.grid(row=2,column=1,sticky="W")
-        self.dni = tk.Entry(win)
+        self.dni = tk.Entry(win,state=self.read)
         self.dni.grid(row=3,column=1,sticky="W")
-        self.cboCarrera = ttk.Combobox(win,width=17)
-        self.cboCarrera['values'] = ["Select..","Ing.Sistemas","Ing.Mecatronica","Ing.Civil","Arquitectura","Periodismo"]
-        self.cboCarrera.current(0)
+        self.cboCarrera = ttk.Combobox(win,width=17,state="disabled",values=("Select..","Ing.Sistemas","Ing.Mecatronica","Ing.Civil","Arquitectura","Periodismo"))
+        self.cboCarrera.current(0)        
         self.cboCarrera.grid(row=1,column=3,sticky="W") 
+        self.cboCarrera.bind("<<ComboboxSelected>>", self.news_election)
 
-        self.facultad = tk.Entry(win,bg="#cdcdcd")
-        self.facultad.grid(row=2,column=3,sticky="W") #Facultad
 
-        self.ciclo = tk.Spinbox(from_=1,to=10,width=5)
+        self.facultad = tk.Entry(win,bg="#cdcdcd",state=self.read)
+        self.facultad.grid(row=2,column=3,sticky="W") #Facultad                
+        
+
+        self.ciclo = tk.Spinbox(from_=1,to=10,width=5,state=self.read)
         self.ciclo.grid(row=3,column=3,sticky="W")
         
-        self.fec_nac = DateEntry(win,background='darkblue', foreground='white', borderwidth=2,date_pattern="dd/MM/yyyy")
+        self.fec_nac = DateEntry(win,width=17,background='darkblue', foreground='white', borderwidth=2,date_pattern="dd/MM/yyyy",state="disabled")
         self.fec_nac.grid(row=4, column=1,sticky="W")
         #self.fec_nac.get_date()
         #print(str(date).split("-"))
@@ -98,26 +100,37 @@ class student:
         ############### BUTTONS ############### (ipadx genera alargamiento horizontal del elemento)
         ttk.Button(win,text="Nuevo",command=self.unlocking_boxes).grid(row=1,column=4,ipadx=50,sticky=tk.W+tk.E)
         ttk.Button(win,text="Grabar",command=self.insert_data).grid(row=2,column=4,sticky=tk.W+tk.E)
-        ttk.Button(win,text="Editar").grid(row=3,column=4,sticky=tk.W+tk.E)
+        ttk.Button(win,text="Editar",command=self.update_student).grid(row=3,column=4,sticky=tk.W+tk.E)
         ttk.Button(win,text="Eliminar",command=self.delete_student).grid(row=4,column=4,sticky=tk.W+tk.E)
 
-    def unlocking_boxes(self):
-        self.nombre = tk.Entry(win,state="normal")
+        ############## SELECTION ################
+        #if(self.cboCarrera['values']=="Ing.Sistemas"):
+            #self.facultad.configure(text="Ingenieria")
+    
+    def news_election(self,event):
+        response=str(event.widget.get())
+        if response.startswith("Ing."):
+            self.facultad.delete(0,tk.END)            
+            self.facultad.insert(0,"Ingenieria")
+        elif response.startswith("Arquitectura"):            
+            self.facultad.delete(0,tk.END)
+            self.facultad.insert(0,"Arquitectura")
+        elif response.startswith("Periodismo"):
+            self.facultad.delete(0,tk.END)
+            self.facultad.insert(0,"Comunicaciones")
+        #print('selected:', event.widget.get())
 
-    # def combo_box_facultad(self):
-    #     self.response = self.cboCarrera.current()
-    #     if self.response == "Ing.Sistemas":
-    #         self.facultad.insert(0,"Ingeniería")
-        # elif self.cboCarrera.current(4):
-        #     self.facultad.insert(0,"Arquitectura")
-        # elif self.cboCarrera.current(5):
-        #     self.facultad.insert(0,"Comunicaciones")
-        # else:
-        #     self.facultad.insert(0,"")
+    def unlocking_boxes(self):                            
+        # if self.cboCarrera.current()==0:
+        #     self.facultad.configure(text="Seleccionado")
+        return self.nombre.configure(state=self.normal), self.apell.configure(state=self.normal), self.dni.configure(state=self.normal), self.fec_nac.configure(state=self.normal), self.cboCarrera.configure(state=self.normal), self.facultad.configure(state=self.normal), self.ciclo.configure(state=self.normal),self.fec_nac.configure(state=self.normal)
+        
+    def locking_boxes(self):
+        return self.nombre.configure(state=self.read), self.apell.configure(state=self.read), self.dni.configure(state=self.read), self.fec_nac.configure(state="disabled"), self.cboCarrera.configure(state="disabled"), self.facultad.configure(state=self.read), self.ciclo.configure(state=self.read)
 
     # def create_table():
     #     try:
-    #         sql = '''CREATE TABLE ESTUDIANTE(        id_est int
+    #         sql = '''CREATE TABLE ESTUDIANTE(        id_est int NOT NULL AUTOINCREMENT
     #                                                 ,nombres varchar(30)
     #                                                 ,apell varchar(30)
     #                                                 ,dni varchar(8)
@@ -125,13 +138,13 @@ class student:
     #                                                 ,carrera varchar(20)
     #                                                 ,facultad varchar(20)
     #                                                 ,ciclo int
-    #                                                 )'''
+    #                                                 ,PRIMARY KEY(id_est))'''
     #         run_query(sql)
     #     except Exception as e:
     #         print(e)
 
     def clear_boxes(self):
-        self.codigo.delete(0,tk.END)
+        #self.codigo.delete(0,tk.END)
         self.nombre.delete(0,tk.END)
         self.apell.delete(0,tk.END)
         self.dni.delete(0,tk.END)
@@ -149,6 +162,7 @@ class student:
             print("Student added successfully")
             self.clear_boxes()            
             self.list_students()
+            self.locking_boxes()
         except Exception as e:
             print(e)
 
@@ -186,15 +200,89 @@ class student:
     #     myConnection.commit()
     #     print(student)
 
-    # def update_student(self,cod,dni):
-    #     sql = f"UPDATE ESTUDIANTE SET dni={dni} WHERE id_est={cod}"
-    #     try:
-    #         myCursor.execute(sql)
-    #         myConnection.commit()
-    #         print("Student was updated successfully!")
-    #         listStudents()
-    #     except Exception as e:
-    #         print(e)
+    def update_student(self):
+    
+        row = self.tree.item(self.tree.selection())['text']
+        old_name = self.tree.item(self.tree.selection())['values'][0]
+        old_lastname = self.tree.item(self.tree.selection())['values'][1]
+        old_dni = self.tree.item(self.tree.selection())['values'][2]
+        old_datebirth = self.tree.item(self.tree.selection())['values'][3]
+        old_career = self.tree.item(self.tree.selection())['values'][4]
+        old_faculty = self.tree.item(self.tree.selection())['values'][5]
+        old_term = self.tree.item(self.tree.selection())['values'][6]
+        self.edit_wind = tk.Toplevel()
+        self.edit_wind.title("Modifying Student")
+        
+        #### OLD ####
+        tk.Label(self.edit_wind,text="Old Name: ").grid(row=0,column=1)
+        tk.Entry(self.edit_wind,textvariable=tk.StringVar(self.edit_wind,value=old_name),state="readonly").grid(row=0,column=2)
+        tk.Label(self.edit_wind,text="Old Last Name: ").grid(row=1,column=1)
+        tk.Entry(self.edit_wind,textvariable=tk.StringVar(self.edit_wind,value=old_lastname),state="readonly").grid(row=1,column=2)
+        tk.Label(self.edit_wind,text="Old DNI: ").grid(row=2,column=1)
+        tk.Entry(self.edit_wind,textvariable=tk.StringVar(self.edit_wind,value=old_dni),state="readonly").grid(row=2,column=2)
+        tk.Label(self.edit_wind,text="Old Birth Date: ").grid(row=3,column=1)
+        tk.Entry(self.edit_wind,textvariable=tk.StringVar(self.edit_wind,value=old_datebirth),state="readonly").grid(row=3,column=2)
+        tk.Label(self.edit_wind,text="Old Career: ").grid(row=4,column=1)
+        tk.Entry(self.edit_wind,textvariable=tk.StringVar(self.edit_wind,value=old_career),state="readonly").grid(row=4,column=2)
+        tk.Label(self.edit_wind,text="Old Faculty: ").grid(row=5,column=1)
+        tk.Entry(self.edit_wind,textvariable=tk.StringVar(self.edit_wind,value=old_faculty),state="readonly").grid(row=5,column=2)
+        tk.Label(self.edit_wind,text="Old Term: ").grid(row=6,column=1)
+        tk.Entry(self.edit_wind,textvariable=tk.StringVar(self.edit_wind,value=old_term),state="readonly").grid(row=6,column=2)
+
+        #### NEW ONE ####
+        tk.Label(self.edit_wind,text="New Name: ").grid(row=0,column=3)
+        new_name = tk.Entry(self.edit_wind)
+        new_name.grid(row=0,column=4)
+        #######
+        tk.Label(self.edit_wind,text="New Last Name: ").grid(row=1,column=3)
+        new_lastname = tk.Entry(self.edit_wind)
+        new_lastname.grid(row=1,column=4)
+        ####### 
+        tk.Label(self.edit_wind,text="New DNI: ").grid(row=2,column=3)
+        new_dni = tk.Entry(self.edit_wind)
+        new_dni.grid(row=2,column=4)
+        #######
+        tk.Label(self.edit_wind,text="New Fec.Nac: ").grid(row=3,column=3)
+        new_fec_nac = DateEntry(self.edit_wind,width=17,background='darkblue', foreground='white', borderwidth=2,date_pattern="dd/MM/yyyy")        
+        new_fec_nac.grid(row=3, column=4)
+        #######
+        def news_election(self,event):
+            response=str(event.widget.get())
+            if response.startswith("Ing."):
+                new_faculty.delete(0,tk.END)            
+                new_faculty.insert(0,"Ingenieria")
+            elif response.startswith("Arquitectura"):            
+                new_faculty.delete(0,tk.END)
+                new_faculty.insert(0,"Arquitectura")
+            elif response.startswith("Periodismo"):
+                new_faculty.delete(0,tk.END)
+                new_faculty.insert(0,"Comunicaciones")
+        #######
+        tk.Label(self.edit_wind,text="New Career: ").grid(row=4,column=3)
+        new_career = ttk.Combobox(self.edit_wind,width=17,values=('Ing.Sistemas','Ing.Mecatronica','Ing.Civil','Arquitectura','Periodismo'))
+        new_career.current(0)
+        new_career.grid(row=4,column=4)
+        new_career.bind("<<ComboboxSelected>>", self.news_election)
+        #######
+        tk.Label(self.edit_wind,text="New Faculty: ").grid(row=5,column=3)
+        new_faculty = tk.Entry(self.edit_wind,bg="#cdcdcd")
+        new_faculty.grid(row=5,column=4)
+        #######
+        tk.Label(self.edit_wind,text="New Term: ").grid(row=6,column=3)
+        new_term = tk.Spinbox(self.edit_wind,from_=1,to=10,width=5)
+        new_term.grid(row=6,column=4)
+                
+        ####### BUTTONS #######
+        tk.Button(self.edit_wind,text="Save").grid(row=7,column=0,columnspan=3,sticky=tk.E+tk.W)
+        tk.Button(self.edit_wind,text="Cancel").grid(row=7,column=3,columnspan=2,sticky=tk.E+tk.W)
+        # sql = f"UPDATE ESTUDIANTE SET dni={dni} WHERE id_est={cod}"
+        # try:
+        #     myCursor.execute(sql)
+        #     myConnection.commit()
+        #     print("Student was updated successfully!")
+        #     listStudents()
+        # except Exception as e:
+        #     print(e)
 
     def delete_student(self):        
         try: 
